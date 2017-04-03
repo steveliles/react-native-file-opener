a9cb43c85971c5100d55895e3dca10d9ec16ab1f
steve <steveliles@gmail.com>
2017-04-03 14:32:08 +0100

    removing ios - not using and it doesn't compile

--- ios/RNFileOpener/RNFileOpener.h
+++
@@ -1,8 +0,0 @@
-#import "RCTBridgeModule.h"
-#import "RCTBridge.h"
-
-@import UIKit;
-
-@interface FileOpener : NSObject <RCTBridgeModule>
-@property (nonatomic) UIDocumentInteractionController * FileOpener;
-@end
